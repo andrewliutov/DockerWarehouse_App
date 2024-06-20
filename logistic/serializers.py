8226eec638a66558e9ad63c7da2149687cd11f40
Andrew Liutov <87426517+andrewliutov@users.noreply.github.com>
2024-06-20 20:56:31 +0300

    Update serializers.py

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -5,7 +5,6 @@ from logistic.models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
 
     class Meta:
         model = Product
@@ -13,7 +12,6 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
 
     class Meta:
         model = StockProduct
@@ -23,21 +21,13 @@ class ProductPositionSerializer(serializers.ModelSerializer):
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
     class Meta:
         model = Stock
         fields = ['id', 'address', 'positions', ]
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
             StockProduct.objects.create(
                 stock=stock,
@@ -48,15 +38,8 @@ class StockSerializer(serializers.ModelSerializer):
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
             StockProduct.objects.update_or_create(
                 stock=stock,
